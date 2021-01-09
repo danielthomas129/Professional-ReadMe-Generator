05acbb867650dbe7dafaa059d4d296ccf1a4c014
Daniel guadalupe <danieltguadalupe@gmail.com>
2021-01-09 13:48:54 -0500

    added index.js content and generate markdown.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,128 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username? (No @ needed)",
+        name: 'username',
+        default: 'connietran-dev',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub username is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: 'readme-generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "If applicable, describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use for the Usage section.",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide guidelines on how other developers can contribute to your project.",
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: "If applicable, provide any tests written for your application and provide examples on how to run them.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+await writeFileAsync('ExampleREADME.md', markdown);
+
+} catch (error) {
+    console.log(error);
+}
+};
+
+init();
+
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
 
-// Function call to initialize app
-init();
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,141 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function generateMarkdown(userResponses, userInfo) {
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  // Generate Table of Contents conditionally based on userResponses
+  let draftToC = `## Table of Contents`;
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  if (userResponses.installation !== '') { draftToC += `
+  * [Installation](#installation)` };
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+  if (userResponses.usage !== '') { draftToC += `
+  * [Usage](#usage)` };
 
-`;
+  if (userResponses.contributing !== '') { draftToC += `
+  * [Contributing](#contributing)` };
+
+  if (userResponses.tests !== '') { draftToC += `
+  * [Tests](#tests)` };
+
+
+  // Generate markdown for the top required portions of the README
+  let draftMarkdown = 
+  `# ${userResponses.title}
+
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  *The what, why, and how:* 
+  
+  ${userResponses.description}
+
+  `
+
+  // Add Table of Contents to markdown
+  draftMarkdown += draftToC;
+ 
+  // Add License section since License is required to Table of Contents
+  draftMarkdown += `
+  * [License](#license)`;
+  
+
+  // Optional Installation section
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  ${userResponses.installation}`
+  };
+  
+
+  // Optional Usage section
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  ${userResponses.usage}`
+  };
+  
+  
+  // Optional Contributing section
+  if (userResponses.contributing !== '') {
+  `
+  
+  ## Contributing
+  
+  *If you would like to contribute it, you can follow these guidelines for how to do so.*
+  
+  ${userResponses.contributing}`
+  };
+  
+
+  // Optional Tests section
+  if (userResponses.tests !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  ${userResponses.tests}`
+  };
+
+
+  // License section is required
+  draftMarkdown +=
+  `
+  
+  ## License
+  
+  ${userResponses.license}
+  `;
+
+
+  // Questions / About Developer section
+  let draftDev = 
+  `
+  ---
+  
+  ## Questions?
+  
+  ![Developer Profile Picture](${userInfo.avatar_url}) 
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@${userInfo.login}](${userInfo.url})
+  `;
+
+  // If GitHub email is not null, add to Developer section
+  if (userInfo.email !== null) {
+  
+  draftDev +=
+  `
+
+  Email: ${userInfo.email}
+
+  `};
+
+  // Add developer section to markdown
+  draftMarkdown += draftDev;
+
+  // Return markdown
+  return draftMarkdown;
+  
 }
 
 module.exports = generateMarkdown;
